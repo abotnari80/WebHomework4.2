6299ef15f46673d0c01714c685df7bbb42547865
abotnari80 <abotnari80@gmail.com>
2022-11-14 01:29:14 +0300

    задача !

---
+++ Task 1/cvp.php
@@ -0,0 +1,8 @@
+<?php
+    $_profession = 'заместитель директора';
+    $_city_country = 'Москва, Россия';
+    $_email = 'abotnari80@gmail.com';
+    $_mobile = '+7(926)-909-09-34';
+
+    echo $_profession
+?>
